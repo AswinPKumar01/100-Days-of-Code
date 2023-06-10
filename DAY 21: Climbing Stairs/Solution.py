6b521f5b7a8e423ffc3685703c1ad7ec067e02bb
Aswin P Kumar <118362715+AswinPKumar01@users.noreply.github.com>
2023-06-10 16:31:04 +0530

    Create Solution.py

---
+++ DAY 21: Climbing Stairs/Solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 0 or n ==1:
+            return 1
+        
+        a,b = 1,1
+        
+
+        for i in range(2,n+1):
+            c = a+b
+            a,b=b,c
+        return b
