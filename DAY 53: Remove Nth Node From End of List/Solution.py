ff2b0df863ec564c587d9c4e65d98f8ac3829d83
Aswin P Kumar <118362715+AswinPKumar01@users.noreply.github.com>
2023-07-15 09:25:10 +0530

    Create Solution.py

---
+++ DAY 53: Remove Nth Node From End of List/Solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        fast = head
+        slow = head
+
+        for i in range(n):
+            fast = fast.next
+            
+        if not fast:
+            return head.next
+
+        while fast.next:
+            slow = slow.next
+            fast = fast.next
+
+        slow.next = slow.next.next
+        return head
