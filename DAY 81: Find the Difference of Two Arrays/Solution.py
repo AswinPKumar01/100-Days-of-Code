b28e0e8c12193f942b7daa3d4a6a974b799b3f99
Aswin P Kumar <118362715+AswinPKumar01@users.noreply.github.com>
2023-09-10 12:23:37 +0530

    Create Solution.py

---
+++ DAY 81: Find the Difference of Two Arrays/Solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        s1, s2 = set(nums1), set(nums2)
+        answer = [list(s1 - s2), list(s2 - s1)]
+        return answer
+                        
+        
